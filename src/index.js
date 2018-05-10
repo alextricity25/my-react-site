06c384dfca8621cf541f9c9157fbbfcfcfe218ac
Miguel Alex Cantu <miguel.can2@gmail.com>
2018-05-10 11:32:42 -0700

    Move things into React Components
    To comply with ReactJS convention, things now have been moved into
    thier respective Components. These Components are used to build
    out the main coverpage.

--- src/index.js
+++ src/index.js
@@ -2,27 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class CoverPage extends React.Component {
+class NamePlate extends React.Component {
+	render() {
+		return (
+			<div className='inner'>
+			  <p className='my-name'>Miguel</p>
+			  <p className='my-name'>Alex Cantu</p>
+			  <Button buttonName='my-life' text='My Life' />
+			  <Button buttonName='my-resume' text='My Resume' />
+			</div>
+		       );
+	}
+}
+
+class CenterStage extends React.Component {
 	render() {
 		return (
 			<div className="outer">
-			  <div className="middle">
-			    <div className="inner">
-			      <p className="my-name">Miguel</p>
-			      <p className='my-name'>  Alex Cantu</p>
-			      <div>
-			        <div className='my-life'>
-			          <button className='my-life-button'>My Life</button>
-				</div>
-				<div className='my-resume'>
-				  <button className='my-resume-button'>My Resume</button>
-				</div>
-			      </div>
-			    </div>
+			  <div className='middle'>
+			    <NamePlate />
 			  </div>
 			</div>
 		       );
 	}
 }
 
-ReactDOM.render(<CoverPage />, document.getElementById('root'));
+class Button extends React.Component {
+	render() {
+		return (
+			<div className={this.props.buttonName}>
+			  <button className={this.props.buttonName + "-button"}>{this.props.text}</button>
+			</div>
+		       );
+	}
+}
+
+ReactDOM.render(<CenterStage />, document.getElementById('root'));

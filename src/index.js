8bff09b58f80ff2444e386c14036076c94486cd9
Miguel Alex Cantu <miguel.can2@gmail.com>
2018-06-29 10:56:55 -0700

    Clean up code a little

--- src/index.js
+++ src/index.js
@@ -9,7 +9,17 @@ class NamePlate extends React.Component {
 		return (
 			<div className='inner'>
 			  <p id='firstname' className='my-name'>Miguel</p>
-			  <p className='my-name'><span className="middle-name" id='middlename'>Alex</span><span id='lastname' className='last-name'> Cantu</span></p>
+			  <p
+			    className='my-name'>
+			    <span
+			      className="middle-name"
+			      id='middlename'>Alex
+			    </span>
+			    <span
+			      id='lastname'
+			      className='last-name'> Cantu
+			    </span>
+			  </p>
 			  <SocialMediaButtonGroup />
 			</div>
 		       );
@@ -28,12 +38,30 @@ class SocialMediaButtonGroup extends React.Component {
 	render() {
 		return (
 			<div id='socialmediabuttons' className='my-life'>
-			  <SocialMediaButton className="fab fa-blogger-b" name="Blog" link="32bc3082196279d5aac3affe5665a8f6.blogspot.com"/>
-			  <SocialMediaButton className="fab fa-twitter" name="Twitter" link="https://twitter.com/MiguelAlexCantu"/>
-			  <SocialMediaButton className="fab fa-google-plus-g" name="Google +" link="https://plus.google.com/u/0/111967829169666843012"/>
-			  <SocialMediaButton className="fab fa-github" name="Github" link="https://github.com/alextricity25"/>
-			  <SocialMediaButton className="fab fa-linkedin-in" name="LinkedIn" link="https://www.linkedin.com/in/miguel-alex-cantu-57440856/"/>
-			  <SocialMediaButton className="fas fa-file-pdf" name="Resume" link={pdfResume}/>
+			  <SocialMediaButton
+			    className="fab fa-blogger-b"
+			    name="Blog"
+			    link="32bc3082196279d5aac3affe5665a8f6.blogspot.com"/>
+			  <SocialMediaButton
+			    className="fab fa-twitter"
+			    name="Twitter"
+			    link="https://twitter.com/MiguelAlexCantu"/>
+			  <SocialMediaButton
+			    className="fab fa-google-plus-g"
+			    name="Google +"
+			    link="https://plus.google.com/u/0/111967829169666843012"/>
+			  <SocialMediaButton
+			    className="fab fa-github"
+			    name="Github"
+			    link="https://github.com/alextricity25"/>
+			  <SocialMediaButton
+			    className="fab fa-linkedin-in"
+			    name="LinkedIn"
+			    link="https://www.linkedin.com/in/miguel-alex-cantu-57440856/"/>
+			  <SocialMediaButton
+			    className="fas fa-file-pdf"
+			    name="Resume"
+			    link={pdfResume}/>
 			</div>
 		       );
 	}
@@ -53,7 +81,12 @@ class SocialMediaButton extends React.Component {
 	}
 	render() {
 		return (
-			<a onMouseOver={this.handleMouseOver.bind(this)} onMouseOut={this.handleMouseOut.bind(this)} href={this.props.link} className={this.props.className}></a>
+			<a
+			  onMouseOver={this.handleMouseOver.bind(this)}
+			  onMouseOut={this.handleMouseOut.bind(this)}
+			  href={this.props.link}
+			  className={this.props.className}>
+			</a>
 		       )
 	}
 }

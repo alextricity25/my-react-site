a6ac2af9b1e9c1f7bc9d1bbaa343329fea988664
Miguel Alex Cantu <miguel.can2@gmail.com>
2019-09-05 13:30:09 -0500

    Added Currently Reading Component
    This commit implements the "Currently Reading" component, which iteratively
    displays a list of book in my currently-reading shelf on goodreads.com
    
    The data is gathered via the goodreads API using the axios library, it's then
    parsed and put on a 5 second interval. Each entry will be displayed for five
    seconds before moving on to the next, cycling back to the beginning once the
    last entry is reached.

--- src/index.js
+++ src/index.js
@@ -5,26 +5,87 @@ import './ridearranger.css';
 import pdfResume from './resume.pdf';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { RideArrangerHome } from './ridearranger';
+import axios from 'axios'
+import xml2json from 'xml2js'
 
 class NamePlate extends React.Component {
 	render() {
 		return (
-			<div className='inner'>
-			  <p id='firstname' className='my-name'>Miguel</p>
-			  <p
-			    className='my-name'>
-			    <span
-			      className="middle-name"
-			      id='middlename'>Alex
-			    </span>
-			    <span
-			      id='lastname'
-			      className='last-name'> Cantu
-			    </span>
-			  </p>
-			  <SocialMediaButtonGroup />
+			<div className='outer'>
+			  <CurrentlyReading />
+			  <div className='inner'>
+			    <p id='firstname' className='my-name'>Miguel</p>
+			    <p
+			      className='my-name'>
+			      <span
+			        className="middle-name"
+			        id='middlename'>Alex
+			      </span>
+			      <span
+			        id='lastname'
+			        className='last-name'> Cantu
+			      </span>
+			    </p>
+			    <SocialMediaButtonGroup />
+			  </div>
 			</div>
 		       );
+	}
+}
+
+class CurrentlyReading extends React.Component {
+	state = {
+		bookList: [],
+		currentBook: "My Reading List",
+		bookNum: 0,
+		error: null
+	}
+
+	componentWillMount(){
+        this.count = 0;
+		const apiKey = process.env.REACT_APP_GOODREADS_API_KEY;
+		var config = {headers: {"X-Requested-With": "XMLHttpRequest"}};
+		axios.get("https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/list/81229333.xml?key=8YvFS344gsnBRBdHhszqlQ&v=2&shelf=currently-reading", config)
+		.then(response => {
+			let err;
+			let _bookList = [];
+			let _bookNum = 0;
+			xml2json.parseString(response.data, function (_err, result) {
+				for (var book of result.GoodreadsResponse.reviews[0].review) {
+					_bookList.push(book.book[0].title[0]);
+					_bookNum++;
+				}
+				console.log(_bookList);
+				err = _err;
+			});
+			this.setState({bookList: _bookList, error: err, bookNum: _bookNum});
+		})
+		.catch(error => {
+			this.setState({error: error})
+		});
+
+		this.timerID = setInterval(
+			() => this.setCurrentBook(),
+			5000
+		);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	setCurrentBook() {
+		this.setState({currentBook: this.state.bookList[this.count % this.state.bookNum], bookList: this.state.bookList});
+		this.count++;
+	}
+	render(){
+		return (
+			<div className='currently_reading_parent'>
+			  <p className='currently-reading-header-text'> Currently Reading:</p>
+			  <p className='currently-reading-text'> {this.state.currentBook} </p>
+			  <p className='currently-reading-footer'> Data from <a href="https://www.goodreads.com/user/show/81229333-miguel-cantu">Goodreads.com</a> </p>
+			</div>
+		);
 	}
 }
 
